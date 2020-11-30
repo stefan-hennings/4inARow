c272f4fb204d73d14f7d4d97ebbe5a54b21dae70
stefan-hennings <stefan.hennings4@gmail.com>
2020-11-30 14:22:53 +0100

    Database shell

--- src/UserDatabase.java
+++ src/UserDatabase.java
@@ -1,6 +1,18 @@
 import java.util.List;
 
 public class UserDatabase {
-    List<User> userList;
-    String fileName = "database.ser";
+    private static List<User> userList;
+    private static final String fileName = "database.ser";
+
+    public static void load() {
+
+    }
+
+    public static void save() {
+
+    }
+
+    public User getUser() {
+        return null;
+    }
 }
