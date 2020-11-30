7e92a8cfdf105b7bb3119666e2c1eb7248ead58a
Oscar Forss <oscar.forss.1@gmail.com>
2020-11-30 14:29:06 +0100

    Added gamestats, scoreImp, User

---
+++ src/GameStats.java
@@ -0,0 +1,39 @@
+public class GameStats implements Score{
+
+    private int wins = 0;
+    private int losses = 0;
+    private int ties = 0;
+
+    @Override
+    public int calculateWinProcent() {
+        return wins/(wins+losses);
+    }
+
+    @Override
+    public void addWin(int i) {
+        this.wins++;
+    }
+
+    @Override
+    public void addLosses(int i) {
+        this.losses++;
+    }
+
+    @Override
+    public void addTie(int i) {
+        this.ties++;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public int getDraws() {
+        return ties;
+    }
+
+}
---
+++ src/Score.java
@@ -0,0 +1,9 @@
+
+public interface Score {
+
+    int calculateWinProcent();
+    void addWin(int i);
+    void addLosses(int i);
+    void addTie(int i);
+
+}
