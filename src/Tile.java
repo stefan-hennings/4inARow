2061d0662f5d2727abd67556f6473b2e5955abb9
PatrikJMelander <patrikjmelander@gmail.com>
2020-11-30 14:07:25 +0100

    Creating a Game Class

---
+++ src/Tile.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Patrik Melander
+ * Date: 2020-11-30
+ * Time: 14:03
+ * Project: 4inARow
+ * Copyright: MIT
+ */
+public enum Tile {
+
+    public final int RED = 1;
+    public final int YELLOW = 2;
+    public final int WHITE = 3;
+}
