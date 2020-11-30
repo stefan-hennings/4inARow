92a8442f6f32e9f61a1f86b3eed8a02e9367998d
PatrikJMelander <patrikjmelander@gmail.com>
2020-11-30 15:29:37 +0100

    attemtLogin readyich, creating LoginSuccessful and loginFail

--- src/LoginMenuPanel.java
+++ src/LoginMenuPanel.java
@@ -1,3 +1,5 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 import javax.swing.*;
 
 public class LoginMenuPanel extends JPanel {
@@ -24,13 +26,21 @@ public class LoginMenuPanel extends JPanel {
     void attemptLogin() {
         User user = new User();
         user = UserDatabase.getUser(userNameField.getText(), passwordField.getText());
+         if (user == null)
+             loginFail();
+         else
+             loginSuccessful(user);
+
     }
 
-    void loginSuccessful() {
-
+    void loginSuccessful(User user) {
+        // Kolla om det var första eller andra användaren som loggade in
+        //
+        // Ladda game panel
     }
 
     void loginFail() {
-
+        System.out.println("Felaktigt användarnamn eller lösenord, försök igen");// fast i GUI
+        //Skicka tillbaka till loginfönstret och be användaren försöka igen
     }
 }
