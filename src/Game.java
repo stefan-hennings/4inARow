e205e7578c6287a80b87083b768814c85e3601b6
stefan-hennings <stefan.hennings4@gmail.com>
2020-12-04 21:57:12 +0100

    Implement placeTile in Game

--- src/Game.java
+++ src/Game.java
@@ -3,11 +3,27 @@ import javax.swing.*;
 public class Game extends JFrame {
     User user1;
     User user2;
-    int tileCounter;
-    int [][] tileGrid;
+    int tileCounter = 0;
+    int [][] tileGrid = new int[6][7];
+    boolean isRedTurn = true;
 
-    public boolean checkWinCondition(){
-        return  true;
+    public void placeTile (int column){
+        for (int row = 0; row < 6; row++) {
+            if (tileGrid[row][column] == Tile.WHITE.getI()) {
+                tileGrid[row][column] = isRedTurn ? Tile.RED.getI() : Tile.YELLOW.getI();
+                tileCounter++;
+                if (hasWon(row, column)) {
+                    processResult();
+                    return;
+                }
+                isRedTurn = !isRedTurn;
+                return;
+            }
+        }
+    }
+
+    public boolean hasWon(int row, int column){
+        for ()
     }
 
     public void processResult(){
